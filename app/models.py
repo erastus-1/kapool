bcf209a6099a3763cad8aebee7615983f3357100
erastus <erastuskariuki15@gmail.com>
2021-01-26 16:50:31 +0300

    add

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User 
+from tinymce.models import HTMLField
+from django.utils import timezone
 from django.db import models
+import datetime as dt
+import cloudinary
 
 # Create your models here.
--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from .forms import *
+from .models import *
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 # Create your views here.
