70c957563561e4be790b55f1c041ca5760fb0424
erastus <erastuskariuki15@gmail.com>
2021-01-26 16:32:24 +0300

    add

--- kapool/settings.py
+++ kapool/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
+    'bootstrap4',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +78,11 @@ WSGI_APPLICATION = 'kapool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'karpool',
+        'USER': 'erastus',
+        'PASSWORD':'Angular2020',
+            }
 }
 
 
