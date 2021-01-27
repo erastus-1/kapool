2acded52306c89735655f6652f544362b18bd6a0
erastus <erastuskariuki15@gmail.com>
2021-01-27 20:19:26 +0300

    add

--- userapp/views.py
+++ userapp/views.py
@@ -7,13 +7,35 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, '/')
+    date = dt.date.today()
+
+    return render(request,'base.html',locals())
+    
+
+@login_required(login_url='/accounts/login/')
+def profile_info(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user=current_user).first()
+
+    return render(request, '/',locals())
 
 
-@login_required(login_url="/accounts/login/")
-def profile(request):
-    return render(request, '/')
+@login_required(login_url='/accounts/login/')
+def profile_update(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.user = current_user
+            add.save()
+        return redirect('/')
+
+    else: 
+        form = UpdateForm()
+    return render(request, '/',{'form':form})
 
 
 @login_required(login_url='/accounts/login/')
